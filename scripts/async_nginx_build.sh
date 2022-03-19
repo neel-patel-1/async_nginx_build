e8ebb62e5ba98d313b65807dd88fa53808efd637
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-03-18 23:01:09 -0500

    testing release

--- scripts/async_nginx_build.sh
+++ scripts/async_nginx_build.sh
@@ -7,6 +7,10 @@ cd $BUILD_DIR/asynch_mode_nginx
 if [ ! -d "$BUILD_DIR/asynch_mode_nginx" ]; then
 	cd $BUILD_DIR
 	git clone --depth 1 --branch v0.4.5 https://github.com/intel/asynch_mode_nginx.git
+	wget http://nginx.org/download/nginx-1.20.1.tar.gz
+	tar -xvzf nginx-1.20.1.tar.gz
+	diff -Naru -x .git nginx-1.20.1 asynch_mode_nginx > async_mode_nginx_1.20.1.patch
+	patch -p0 < async_mode_nginx_1.20.1.patch
 fi
 
 ./configure \
--- scripts/buildRelease.sh
+++ scripts/buildRelease.sh
@@ -18,7 +18,7 @@ $ROOT_DIR/scripts/qatengine_build.sh
 #use benchmarking qat device confs
 $ROOT_DIR/scripts/benchmark_qat_conf.sh
 #use benchmarking nginx conf
-$ROOT_DIR/scripts/
+$ROOT_DIR/scripts/cp_nginx_conf.sh
 
 #restart qat devices
 sudo service qat_service restart
