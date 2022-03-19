71220c113a80a4b290aa88840bc94b66085c9eda
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-03-19 15:56:13 -0500

    config scripts

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 newroot=$(echo "$(pwd)" | sed 's/\//\\\//g')
 grep -rn "^export ROOT_DIR" * | awk -F':' '{print $1}' | grep -E 'scripts.*\.(sh|src|source)$' | xargs sed -i "s/export ROOT_DIR=.*/export ROOT_DIR=${newroot}/"
+
+newopenssl=${newroot}\\/builds\\/openssl
+newopenssllib=$newopenssl\\/lib
+newopensslengines=$newopenssllib\\/engines-1.1
+sed -i -e "s/export OPENSSL_ENGINES=.*/export OPENSSL_ENGINES=${newopensslengines}/" -e "s/export LD_FLAGS=.*/export LD_FLAGS=${newopenssl}/" $(pwd)/scripts/qtls_libs.src
+
+
+sed -i -e "s/export LD_LIBRARY_PATH=.*(:.*:.*_)/export LD_LIBRARY_PATH=${newopensslengines}:/" -e "s/export LD_FLAGS=.*/export LD_FLAGS=${newopenssl}/" $(pwd)/scripts/qtls_libs.src
+
+
+[ ! -d "$(pwd)/Builds" ] && mkdir $(pwd)/Builds
+
+newroot=$(echo "$(pwd)" | sed 's/\//\\\//g')
+grep -rn "^export ROOT_DIR" * | awk -F':' '{print $1}' | grep -E 'scripts.*\.(sh|src|source)$' | xargs sed -i "s/export ROOT_DIR=.*/export ROOT_DIR=${newroot}/"
