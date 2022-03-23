d07d6802bc8372bb789287f2186cc526c593152e
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-03-23 11:57:41 -0500

    http file generation

--- scripts/build_ipp_crypto.sh
+++ scripts/build_ipp_crypto.sh
@@ -12,5 +12,6 @@ git checkout ippcp_2020u3
 cd sources/ippcp/crypto_mb
 CRYPTO_DIR=$(cmake . -Bbuild -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/crypto_mb/2020u3 | grep 'Build files' | awk '{print $8}')
 cd $CRYPTO_DIR
+[[ "$(grep -E 'export CRYPTO_DIR=.*' scripts/async_libsrcs.source)" == "" ]] && echo "export CRYPTO_DIR=$CRYPTO_DIR" >> $ROOT_DIR/scripts/async_libsrcs.source
 make -j 35
 sudo make install -j 35
---
+++ scripts/gen_http_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export ROOT_DIR=/home/n869p538/patched_async_mode_nginx
+source $ROOT_DIR/scripts/async_libsrcs.source
+
+
+[[ ! -d "$ROOT_DIR/html_files" ]] && mkdir $ROOT_DIR/html_files
+
+
+cd $ROOT_DIR/html_files
+
+declare -a sizes=("256K" "4K" "16K" "64K" "128K")
+
+for size in "${sizes[@]}";
+do
+	echo $size
+	head -c $size < /dev/urandom > file_$size.txt
+done
+
+[[ -d "$DEFAULT_NGINX_BUILD" ]] && sudo cp -r $ROOT_DIR/html_files/* $DEFAULT_NGINX_BUILD/html
+[[ -d "$NGINX_INSTALL_DIR" ]] && sudo cp -r $ROOT_DIR/html_files/* $NGINX_INSTALL_DIR/html
