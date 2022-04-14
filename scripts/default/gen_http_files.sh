0ffbbfa54cc7e1f399f349e2f0ad42f2aed2403c
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-04-14 00:52:19 -0500

    refactoring

---
+++ scripts/default/gen_http_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export ROOT_DIR=/home/n869p538/async_nginx_build
+source $ROOT_DIR/scripts/async_libsrcs.source
+
+
+[[ ! -d "$ROOT_DIR/html_files" ]] && mkdir $ROOT_DIR/html_files
+
+
+cd $ROOT_DIR/html_files
+
+declare -a sizes=("256K" "4K" "16K" "64K" "128K")
+
+for size in "${sizes[@]}";
+do
+	#echo $size
+	head -c $size < /dev/urandom > file_$size.txt
+done
+
+[[ -d "$DEFAULT_NGINX" ]] && sudo cp -r $ROOT_DIR/html_files/* $DEFAULT_NGINX/html
