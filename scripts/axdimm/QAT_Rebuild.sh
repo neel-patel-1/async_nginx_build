e23168e5f2abc0f5f4d3ed21f4a5042e8a6c8a41
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-04-15 11:18:53 -0500

    rebuild qat sw

---
+++ scripts/axdimm/QAT_Rebuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export ROOT_DIR=/home/n869p538/async_nginx_build
+source $ROOT_DIR/scripts/async_libsrcs.source
+
+if [ ! -D "${AXDIMM_DIR}/QAT_Engine" ]; then
+	cd ${AXDIMM_DIR}
+	cd QAT_Engine
+	./autogen.sh
+	#only link against Multi-Buffer
+	LDFLAGS="-L$AXDIMM_DIR/intel-ipsec-mb/lib " \
+	CPPFLAGS="-I$AXDIMM_DIR/intel-ipsec-mb/lib/include \
+	-I$AXDIMM_DIR/crypto_mb/2020u3/include/crypto_mb \
+	-I$AXDIMM_DIR/crypto_mb/2020u3/include" ./configure \
+	--enable-qat_sw \
+	--with-openssl_install_dir=${AXDIMM_DIR}/openssl \
+	--with-openssl_dir=${AXDIMM_DIR}/openssl \
+	--disable-qat_hw \
+	--enable-qat_debug #keep debug for now to verify our sw changes
+	cp -f ${ROOT_DIR}/axdimm_aes_gcm/qat_sw_gcm.c .
+	PERL5LIB=$AXDIMM_DIR/openssl make -j
+	sudo PERL5LIB=$AXDIMM_DIR/openssl make install
+
+fi
