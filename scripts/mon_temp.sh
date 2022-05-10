9c69601b9a8340ba8a6965c47f317f6c5ff0a288
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-05-10 15:43:07 -0500

    temperature monitoring

--- scripts/mon_temp.sh
+++ scripts/mon_temp.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+export ROOT_DIR=/home/n869p538/async_nginx_build
+source $ROOT_DIR/scripts/async_libsrcs.source
+
 [ -z "$qat_thresh" ] && qat_thresh=50
 [ -z "$mellanox_thresh" ] && mellanox_thresh=50
 [ -z "$qat_name" ] && qat_name="pch_lewisburg-virtual-0"
 [ -z "$mel_dev" ] && mel_devs=( "/dev/mst/mt41682_pciconf0" "/dev/mst/mt41682_pciconf0.1" )
 
-qat_temp=$( sensors | sed -n "/$qat_name/{N;N;p}" | tail -n 1 | grep -Eo '[+|-][0-9]*' | grep -Eo '[0-9]*' )
 
 [ ! -d "/dev/mst" ] && sudo mst start
 
-for i in "${mel_devs[@]}"; do
-	mtemp=$(sudo mget_temp -d $i)
-	echo "mellanox dev: $mtemp"
+while true; do
+	for i in "${mel_devs[@]}"; do
+		mtemp=$(sudo mget_temp -d $i)
+		echo "mellanox dev: $mtemp"
+		if [ "$mtemp" -gt "$mellanox_thresh" ]; then
+			echo "maxmimum mellanox temp exceeded"
+		fi
+	done
+
+	qat_temp=$( sensors | sed -n "/$qat_name/{N;N;p}" | tail -n 1 | grep -Eo '[+|-][0-9]*' | grep -Eo '[0-9]*' )
+	echo "qat temp: $qat_temp"
+
+	if [ "$qat_temp" -gt "$qat_thresh" ]  ; then
+			echo "maxmimum qat device temp exceeded"
+	fi
+	sleep 2
+
 done
-
-echo "qat temp: $qat_temp"
-
-if [ "$qat_temp" -gt $qat_thresh ]; then
-fi
