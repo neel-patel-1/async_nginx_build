ca5a271d0ff667b61583033b6cc04c951c3116db
Neel Patel <n869p538@ittc.ku.edu>
2022-05-13 12:33:55 -0500

    removed sudo

--- scripts/mon_temp.sh
+++ scripts/mon_temp.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
-
 export ROOT_DIR=/home/n869p538/async_nginx_build
 source $ROOT_DIR/scripts/async_libsrcs.source
 
-[ -z "$qat_thresh" ] && qat_thresh=55
-[ -z "$mellanox_thresh" ] && mellanox_thresh=55
+[ -z "$qat_thresh" ] && qat_thresh=50
+[ -z "$mellanox_thresh" ] && mellanox_thresh=50
 [ -z "$qat_name" ] && qat_name="pch_lewisburg-virtual-0"
 [ -z "$mel_dev" ] && mel_devs=( "/dev/mst/mt41682_pciconf0" "/dev/mst/mt41682_pciconf0.1" )
 
 
-[ ! -d "/dev/mst" ] && sudo mst start
+[ ! -d "/dev/mst" ] && mst start
 
 while true; do
 	for i in "${mel_devs[@]}"; do
-		mtemp=$(sudo mget_temp -d $i)
-		echo "mellanox dev: $mtemp"
+		mtemp=$(mget_temp -d $i)
 		if [ "$mtemp" -gt "$mellanox_thresh" ]; then
-			echo "maxmimum mellanox temp exceeded"
-			sudo shutdown now
+			shutdown now
 		fi
 	done
 
 
 	for i in /dev/mst/*; do
-		mtemp=$(sudo mget_temp -d $i)
-		echo "mellanox dev: $mtemp"
+		mtemp=$(mget_temp -d $i)
 		if [ "$mtemp" -gt "$mellanox_thresh" ]; then
-			echo "maxmimum mellanox temp exceeded"
-			sudo shutdown now
+			shutdown now
 		fi
 	done
 	
 
 	qat_temp=$( sensors | sed -n "/$qat_name/{N;N;p}" | tail -n 1 | grep -Eo '[+|-][0-9]*' | grep -Eo '[0-9]*' )
-	echo "qat temp: $qat_temp"
 
 	if [ "$qat_temp" -gt "$qat_thresh" ]  ; then
-			echo "maxmimum qat device temp exceeded"
-			sudo shutdown now
+			shutdown now
 	fi
 	sleep 2
 
