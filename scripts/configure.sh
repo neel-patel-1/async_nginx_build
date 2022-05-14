a6ad1e6e0720b22d709870b628d139c268eb3e2f
Neel Patel <n869p538@ittc.ku.edu>
2022-05-14 11:50:30 -0500

    offload char dev tree

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 newroot=$(echo "$(pwd)" | sed 's/\//\\\//g')
 grep -rn "^export ROOT_DIR" * | awk -F':' '{print $1}' | grep -E 'scripts.*\.(sh|src|source)$' | xargs sed -i "s/export ROOT_DIR=.*/export ROOT_DIR=${newroot}/"
+sudo apt install libpcre3-dev
 
 exit
 
