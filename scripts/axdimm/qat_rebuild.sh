2529234f5d764a137a2c335144730f03bf6545b8
Neel Patel <n869p538@ittc.ku.edu>
2022-05-19 21:20:29 -0500

    quick rebuild

--- scripts/axdimm/qat_rebuild.sh
+++ scripts/axdimm/qat_rebuild.sh
@@ -3,5 +3,17 @@ export ROOT_DIR=/home/n869p538/async_nginx_build
 source $ROOT_DIR/scripts/async_libsrcs.source
 
 cd ${AXDIMM_DIR}/qat_cache_flush
+LDFLAGS=\"-L$AXDIMM_DIR/intel-ipsec-mb/lib  \
+-L$AXDIMM_DIR/lib\" \
+CPPFLAGS=\"-I${AXDIMM_DIR}/intel-ipsec-mb/lib/include \
+-I${AXDIMM_DIR}/include \
+-I$AXDIMM_DIR/intel-ipsec-mb/lib \
+-I$AXDIMM_DIR/crypto_mb/2020u3/include/crypto_mb \
+-I$AXDIMM_DIR/crypto_mb/2020u3/include\" ./configure \
+--enable-qat_sw \
+--with-openssl_install_dir=${AXDIMM_DIR}/openssl \
+--with-openssl_dir=${AXDIMM_DIR}/openssl \
+--disable-qat_hw \
+--enable-qat_debug
 PERL5LIB=$AXDIMM_DIR/openssl make -j $(( `nproc` / 2 ))
 sudo PERL5LIB=$AXDIMM_DIR/openssl make install -j $(( `nproc` / 2 ))
