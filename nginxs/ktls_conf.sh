e3daa2439e64c8af041ddeb62f140b6aad3545e3
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-05-29 22:13:22 -0500

    rebuilding the default w openssl3 -- apples to apples w/ ktls

---
+++ nginxs/ktls_conf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+>&2 echo "[info] ktls sendfile server... "
+
+sudo cp -f ${ROOT_DIR}/ktls_nginx_conf/nginx.conf ${KTLS_NGINX}/conf/nginx.conf
+
+if [ -z "$( grep worker_processes ${KTLS_NGINX}/conf/nginx.conf )" ]; then
+	sudo sed -i -E "/number of cores/a worker_processes ${cores};" ${KTLS_NGINX}/conf/nginx.conf
+else
+	sudo sed -i -E "s/(worker_processes) (.*)(;)/\1 $cores\3/g" ${KTLS_NGINX}/conf/nginx.conf
+fi
+
+if [ -z "$( grep worker_cpu_affinity ${KTLS_NGINX}/conf/nginx.conf )" ]; then
+	sudo sed -i -E "/worker_processes/a $masks" ${KTLS_NGINX}/conf/nginx.conf
+else
+	sudo sed -i -E "s/(worker_cpu_affinity) (.*)(;)/$masks/g" ${KTLS_NGINX}/conf/nginx.conf
+fi
+
+sudo ${KTLS_NGINX}/sbin/nginx -t
+sudo ${KTLS_NGINX}/sbin/nginx
---
+++ scripts/default/rebuild_default.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export ROOT_DIR=/home/n869p538/async_nginx_build
+source $ROOT_DIR/scripts/async_libsrcs.source
+
+if [ ! -d "${DEFAULT_DIR}" ] || [ ! -f "${DEFAULT_DIR}/openssl-3.0.0.tar.gz" ] || [ ! -d "${DEFAULT_DIR}/openssl-3.0.0" ] || [ ! -f "${DEFAULT_DIR}/nginx-1.21.4.tar.gz" ] || [ ! -d "${DEFAULT_DIR}/nginx-1.21.4" ] || [ ! -f "${DEFAULT_DIR}/nginx_build/sbin/nginx" ]; then 
+	echo "DEFAULT Directory not setup ..." 
+	${ROOT_DIR}/scripts/default/build_default.sh >/dev/null
+else
+	sudo rm -rf ${DEFAULT_DIR}/nginx-1.21.4/Makefile ${DEFAULT_DIR}/nginx_build
+	${ROOT_DIR}/scripts/default/build_default.sh $*
+fi
+
