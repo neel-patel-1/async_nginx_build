7e851e28759a7fda7b1298ad53648d93b125cd03
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-07-02 10:37:08 -0500

    new http file gen

--- scripts/gen_http_files.sh
+++ scripts/gen_http_files.sh
@@ -6,9 +6,9 @@ source $ROOT_DIR/scripts/async_libsrcs.source
 size=${1}
 
 for i in "${all_nginxs[@]}"; do
-	if [ ! -f "${!i}/html/file_${size}" ]; then
+	if [ ! -f "${!i}/html/file_${size}.txt" ]; then
 		>&2 echo "adding file_${size} to ${i}"
-		head -c $size < /dev/urandom > file_$size.txt
+		head -c $size < /dev/urandom | sudo tee ${!i}/html/file_$size.txt >/dev/null
 	else
 		>&2 echo "file_${size} already present in ${i}"
 	fi	
