610aebad0c173b201c916f5c6097e1fffa24570e
Neel Patel <n869p538@ittc.ku.edu>
2022-07-26 10:23:45 -0500

    iperf clinet

---
+++ scripts/iperf/iperf_client.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#set root directory
+export ROOT_DIR=/home/n869p538/async_nginx_build
+
+#get global variables
+source ${ROOT_DIR}/scripts/async_libsrcs.source
+
+[ ! -f "${iperf_dir}/newreq.pem" ] || [ ! -f "${iperf_dir}/key.pem" ] && openssl req -x509 -newkey rsa:2048 -keyout ${iperf_dir}/key.pem -out ${iperf_dir}/newreq.pem -days 365 -nodes
+cd ${iperf_dir}
+
+>&2 echo "[info] AXDIMM iperf client..."
+sudo env \
+OPENSSL_ENGINES=$AXDIMM_ENGINES \
+LD_LIBRARY_PATH=$AXDIMM_OSSL_LIBS:$AXDIMM_DIR/lib \
+$offload_iperf --tls=qat -c ${remote_ip} -t 60 -i 5 -y C
