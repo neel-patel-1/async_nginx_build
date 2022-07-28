fd2e3f907af19e0486d6762de69cfa90b5884bde
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-07-28 12:23:39 -0500

    benchmark scripts

---
+++ scripts/kvs/build_bench.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
+source $ROOT_DIR/scripts/async_libsrcs.source
+
+[ ! -d $ROOT_DIR/kv_bench ] && mkdir $ROOT_DIR/kv_bench
+cd $ROOT_DIR/kv_bench
+
+[ ! -d "mc-crusher" ] && git clone https://github.com/memcached/mc-crusher.git \
+       && cd mc-crusher && git reset --hard 7ca3f14c128495a509cd0aaff464545377fa5cee
+
+cd $mc_bench_dir
+[ ! -f "$mc_bench_bin" ] && make tls -j 4 
