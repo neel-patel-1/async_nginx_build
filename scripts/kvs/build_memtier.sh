61e51b43f2bec576b9a445031314af07f1f97f91
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-07-30 00:36:14 -0500

    memtier bin

--- scripts/kvs/build_memtier.sh
+++ scripts/kvs/build_memtier.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export ROOT_DIR=/home/n869p538/async_nginx_build
+export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
 source $ROOT_DIR/scripts/async_libsrcs.source
 
 
@@ -7,3 +7,10 @@ source $ROOT_DIR/scripts/async_libsrcs.source
 cd $ROOT_DIR/kv_bench
 
 [ ! -d "memtier_benchmark" ] && git clone --branch 1.3.0 --depth 1 https://github.com/RedisLabs/memtier_benchmark.git
+cd memtier_benchmark
+if [ ! -f "$ROOT_DIR/memtier_build/bin/memtier_benchmark" ]; then
+	autoreconf -ivf
+	./configure --prefix=$ROOT_DIR/memtier_build
+	make -j 4
+	make install -j 4
+fi
