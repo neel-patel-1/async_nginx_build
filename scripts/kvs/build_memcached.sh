da46c8a52afd45697e4c312d46fe1d3814de6c9c
Neel Patel <n869p538@ittc.ku.edu>
2022-08-01 00:55:30 -0500

    axdimm memcached alterations

--- scripts/kvs/build_memcached.sh
+++ scripts/kvs/build_memcached.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
+export ROOT_DIR=/home/n869p538/async_nginx_build
 source $ROOT_DIR/scripts/async_libsrcs.source
 
-[ ! -d $ROOT_DIR/kvs ] && mkdir $ROOT_DIR/kvs
+[ ! -d "$ROOT_DIR/kvs" ] && mkdir $ROOT_DIR/kvs
 cd $ROOT_DIR/kvs
 [ ! -f "libevent-2.1.12-stable.tar.gz" ] && wget https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz
 [ ! -d "libevent-2.1.12-stable" ]  && tar -xzf libevent-2.1.12-stable.tar.gz
@@ -16,8 +16,7 @@ fi
 cd $ROOT_DIR/kvs
 [ ! -f "memcached-1.6.15.tar.gz" ] &&  wget http://www.memcached.org/files/memcached-1.6.15.tar.gz
 [ ! -d "memcached-1.6.15" ] && tar -xzf memcached-1.6.15.tar.gz
-[ ! -d "memcached-1.6.15_patched" ] && patch -s -p0 < offload.patch && mv memcached-1.6.15 memcached-1.6.15_patched
-cd memcached-1.6.15_patched
+cd memcached-1.6.15
 if [ ! -f "$(pwd)/../memcached_build/bin/memcached" ]; then
 	# -Werror flag in makefile is preventing engine build due to warnings as errors , do we need to sed it out?
 	./configure --prefix=$(pwd)/../memcached_build \
