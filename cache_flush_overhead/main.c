c9cb1ca4919513662b380f67574305c9a93c6f7c
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-10-09 15:45:43 -0500

    flush

--- cache_flush_overhead/main.c
+++ cache_flush_overhead/main.c
@@ -21,11 +21,50 @@
 
 #include <papi.h>
 
-#define BUF_MAX 16 * 1024 * 1024 
+#define BUF_MAX 4 * 1024
+#define INC 512
+
+int flush_frac(){
+	printf("IN_DRAM, IN_CACHE, ns_TO_FLUSH, cycles_TO_FLUSH\n" );
+	int iter=10;
+	long_long s_cycle, e_cycle;
+	long_long s_nano, e_nano;
+	long_long nsec_avg, cycle_avg;
+
+	for (int i=0; i<=BUF_MAX; i+=INC){
+		/* microsecs and cycles */
+		nsec_avg=0;
+		cycle_avg=0;
+
+		for (int k=0; k<iter; k++){
+			/* allocate buffer -- it starts in the cache */
+			char * buf = (char *) malloc( BUF_MAX );
+
+			/*preflush the beginning i bytes of the buffer */
+			for ( int j=0; j<i; j+=64 ){
+				_mm_clflush( buf + j );
+			}
+
+			/* measure time to flush now that some are flushed */
+			long_long s_cycle = PAPI_get_real_cyc();
+			long_long s_nano = PAPI_get_real_nsec();
+			for (int j=0; j<BUF_MAX; j+=64){
+				_mm_clflush(buf + j);
+			}
+			e_nano = PAPI_get_real_nsec();
+			e_cycle = PAPI_get_real_cyc();
+			cycle_avg+=(e_cycle - s_cycle);
+			nsec_avg+=(e_nano - s_nano);
+		}
+		printf("%d, %d, %lld, %lld\n", i, BUF_MAX-i, nsec_avg/iter, cycle_avg/iter);
+	}
+}
 
 int main(){
 
 
+	flush_frac();
+	return 0;
 
 	printf("in_cache_buffer_size, num_cycles_spent_flushing, ns_spent_flushing\n" );
 	for (int i=1; i<=BUF_MAX; i=i*2){
