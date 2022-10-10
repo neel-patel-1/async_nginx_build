c2daf20e2b7e4b2fd68cc2f2e0be18ee6cc87a79
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-10-10 10:53:23 -0500

    ktls memcached

---
+++ scripts/kvs/build_ktls_memcached.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
+#source $ROOT_DIR/scripts/async_libsrcs.source
+
+[ ! -d "$ROOT_DIR/kvs" ] && mkdir $ROOT_DIR/kvs
+cd $ROOT_DIR/kvs
+[ ! -f "libevent-2.1.12-stable.tar.gz" ] && wget --no-check-certificate https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz
+[ ! -d "libevent-2.1.12-stable" ]  && tar -xzf libevent-2.1.12-stable.tar.gz
+if [ ! -d "$(pwd)/libevent_build" ]; then
+	cd libevent-2.1.12-stable
+	./configure --prefix=$(pwd)/../libevent_build
+	make -j 4
+	make install -j 4
+fi
+
+cd $ROOT_DIR/kvs
+#[ ! -d "memcached" ] && git clone --depth 1 --branch 1.6.17 https://github.com/memcached/memcached
+[ ! -f "memcached-1.6.16.tar.gz" ] && wget http://www.memcached.org/files/memcached-1.6.16.tar.gz
+[ ! -d "memcached-1.6.16" ] && tar -xzf memcached-1.6.16.tar.gz
+cd memcached-1.6.16
+if [ ! -f "$(pwd)/../ktls_mem_build/bin/memcached" ]; then
+	# -Werror flag in makefile is preventing engine build due to warnings as errors , do we need to sed it out?
+	./configure --prefix=$(pwd)/../ktls_mem_build \
+	LDFLAGS=-L/home/n869p538/wrk_offloadenginesupport/client_ossls/openssl-3.0.0 \
+	--enable-tls \
+	--with-libssl=/home/n869p538/wrk_offloadenginesupport/client_ossls/openssl-3.0.0 \
+	--with-libevent=$(pwd)/../libevent_build
+	#sed -i -E 's/CFLAGS = (.*)-Werror(.*)/CFLAGS = \1\2/g' Makefile
+
+	make -j 4
+	make install -j 4
+fi
+cd ../
+[ ! -f "openssl.cnf" ] && cp -r /etc/ssl/openssl.cnf .
+export OPENSSL_CONF=$(pwd)/openssl.cnf
+[ ! -f "cert.pem" ] || [ ! -f "key.pem" ] && openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes
---
+++ scripts/kvs/ktls_memcached.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
+source $ROOT_DIR/scripts/async_libsrcs.source
+
+export LD_LIBRARY_PATH=$KTLS_OSSL:$LD_LIBRARY_PATH
+
+[ ! -f "${ktls_mem_bin}" ] && echo "missing ktls memcached binary" && exit
+
+${ktls_mem_bin} -l notls:*:5001,*:5002 -Z -t 8  -o ssl_chain_cert=${kvs_dir}/cert.pem,ssl_key=${kvs_dir}/key.pem
