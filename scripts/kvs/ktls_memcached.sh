b70bee08d9e480d68be061fddd01d478973fd6c2
Neel Patel <n869p538@ittc.ku.edu>
2022-10-10 12:11:05 -0500

    -o ssl_kernel_tls

--- scripts/kvs/ktls_memcached.sh
+++ scripts/kvs/ktls_memcached.sh
@@ -6,4 +6,4 @@ export LD_LIBRARY_PATH=$KTLS_OSSL:$LD_LIBRARY_PATH
 
 [ ! -f "${ktls_mem_bin}" ] && echo "missing ktls memcached binary" && exit
 
-${ktls_mem_bin} -l notls:*:5001,*:5002 -Z -t 8  -o ssl_chain_cert=${kvs_dir}/cert.pem,ssl_key=${kvs_dir}/key.pem
+${ktls_mem_bin} -l notls:*:5001,*:5002 -Z -t 8  -o ssl_kernel_tls,ssl_chain_cert=${kvs_dir}/cert.pem,ssl_key=${kvs_dir}/key.pem
