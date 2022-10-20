583b3417bb714bb2cbc99e8c8d8f3f45886e4709
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-10-20 10:11:04 -0500

    updated thread count

--- scripts/kvs/qtls_memcached.sh
+++ scripts/kvs/qtls_memcached.sh
@@ -7,4 +7,4 @@ export LD_LIBRARY_PATH=$QTLS_LIBS:$QTLS_ENGINES
 
 [ ! -f "${qtls_mem_bin}" ] && exit
 
-sudo ${qtls_mem_bin} --user=n869p538 -l notls:*:5001,*:5002 -Z -t 8  -o ssl_chain_cert=${kvs_dir}/cert.pem,ssl_key=${kvs_dir}/key.pem
+sudo ${qtls_mem_bin} --user=n869p538 -l notls:*:5001,*:5002 -Z -t 20 -o ssl_chain_cert=${kvs_dir}/cert.pem,ssl_key=${kvs_dir}/key.pem
