d84c5c1f0e641c02a20d80d9d1d8d3826df085f0
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-11-08 19:09:32 -0600

    build emul with openssl

--- nginx_compress_emul/emul_build.sh
+++ nginx_compress_emul/emul_build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
+source ../scripts/async_libsrcs.source
 sudo rm -rf nginx_gzip_build
 cd nginx-gzip_offload
-./configure --prefix=$(pwd)/../nginx_gzip_build 
+./configure --prefix=$(pwd)/../nginx_gzip_build \
+	--with-ld-opt="-L ${DEFAULT_DIR}/openssl" \
+	--with-http_ssl_module \
+	--with-http_slice_module \
+	--with-openssl=${DEFAULT_DIR}/openssl-3.0.0 \
+	--with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC' \
+	--with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie'
 make -j 30
 make -j 30 install 
 cd ../
-sudo cp nginx_default.conf nginx_gzip_build/conf/nginx.conf
+cp nginx_default.conf nginx_gzip_build/conf/nginx.conf
+cp -r localhost.* nginx_gzip_build/conf/
 ps aux | grep nginx | awk '{print $2}' | xargs sudo kill -s 2
 sudo ./nginx_gzip_build/sbin/nginx
 f_sz=( "4K" "16K" "32K" )
--- nginx_compress_emul/start_default_gzip.sh
+++ nginx_compress_emul/start_default_gzip.sh
@@ -2,4 +2,7 @@
 ps aux | grep nginx | awk '{print $2}' | xargs sudo kill -s 2
 sleep 0.3
 sudo cp nginx_original.conf nginx_default_build/conf/nginx.conf
-sudo ./nginx_default_build/sbin/nginx
+
+NCORES=10
+sudo rdtset -r 1-$NCORES -t "l3=0x80;cpu=1-$NCORES" -c 1-$NCORES -k \
+	./nginx_default_build/sbin/nginx
