79ed111a1903319e990b1f1e126796c2c0ada68a
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-11-14 18:47:31 -0600

    emul_build

--- nginx_compress_emul/emul_build.sh
+++ nginx_compress_emul/emul_build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
 source ../scripts/async_libsrcs.source
+../scripts/L5P_DRAM_Experiments/setup_server.sh
 sudo rm -rf nginx_gzip_build
-cd nginx-gzip_offload
-./configure --prefix=$(pwd)/../nginx_gzip_build \
-	--with-ld-opt="-L ${DEFAULT_DIR}/openssl" \
-	--with-http_ssl_module \
-	--with-http_slice_module \
-	--with-openssl=${DEFAULT_DIR}/openssl-3.0.0 \
-	--with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC' \
-	--with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie'
+cd nginx-1.23.1
+./configure --prefix=$(pwd)/../nginx_gzip_build  --with-debug --add-module=$(pwd)/../nginx-nmp-overhead-module
 make -j 30
 make -j 30 install 
 cd ../
@@ -17,8 +12,4 @@ cp nginx_default.conf nginx_gzip_build/conf/nginx.conf
 cp -r localhost.* nginx_gzip_build/conf/
 ps aux | grep nginx | awk '{print $2}' | xargs sudo kill -s 2
 sudo ./nginx_gzip_build/sbin/nginx
-f_sz=( "4K" "16K" "32K" )
-for i in "${f_sz[@]}"; do
-	head -c ${i} < /dev/urandom | sudo tee nginx_gzip_build/html/rand_file_${i}.txt >/dev/null
-	head -c ${i} < /dev/zero | sudo tee nginx_gzip_build/html/zero_file_${i}.txt >/dev/null
-done
+../scripts/L5P_DRAM_Experiments/setup_compression_corpus.sh 16K
