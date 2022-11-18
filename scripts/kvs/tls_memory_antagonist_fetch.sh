b54de9a62e7fb57f6ac81000410a97cfa527a382
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-11-18 01:27:30 -0600

    tls kvs workload

--- scripts/kvs/tls_memory_antagonist_fetch.sh
+++ scripts/kvs/tls_memory_antagonist_fetch.sh
@@ -4,11 +4,21 @@ source $ROOT_DIR/scripts/async_libsrcs.source
 
 export LD_LIBRARY_PATH=${KTLS_OSSL}:${LD_LIBRARY_PATH}
 
-MEM_S=$(( 1 * 1024 * 1024 * 1024 ))
-VSIZE=$(( 1 * 1024 ))
-SET_P="--ratio=0:1 --key-pattern=S:S"
-EXTRA="--hide-histogram"
-KEY_RANGE="--key-minimum=200 --key-maximum=$(( 200 + (MEM_S / VSIZE) )) -n allkeys"
-DATA=" --random-data --data-size=${VSIZE}"
+N_OPS=100000000
+MEM_S=${1}
+VSIZE=${2}
+TDS=${3}
+CLIS=${4}
 
-${memtier_bin} --tls --tls-skip-verify -s ${remote_mem_ip} -p 5002 -t 4 -P memcache_text $DATA $SET_P $EXTRA $KEY_RANGE
+[ -z "${1}" ] && MEM_S=$(( 5 * 1024 * 1024 * 1024 ))
+[ -z "${2}" ] && VSIZE=$(( 1 * 1024 ))
+[ -z "${3}" ] && TDS=10
+[ -z "${4}" ] && CLIS=50
+
+SET_P="--ratio=0:1 --key-pattern=R:R"
+EXTRA="--hide-histogram"
+KEY_RANGE="--key-minimum=200 --key-maximum=$(( 200 + (MEM_S / VSIZE) ))"
+DATA=" --data-size=${VSIZE}"
+TD_CLI_OPS="-t $TDS -c $CLIS -n 1000000" #$(( N_OPS / (TDS * CLIS) ))"
+
+${memtier_bin} --tls --tls-skip-verify -s ${remote_mem_ip} -p 5002 -P memcache_text $SET_P $EXTRA $KEY_RANGE $TD_CLI_OPS
