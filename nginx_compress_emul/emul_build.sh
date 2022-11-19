8aea59d8752d1809b28fd6aa67533ccf527466b5
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-11-19 15:02:11 -0600

    qat nginx confs

--- nginx_compress_emul/emul_build.sh
+++ nginx_compress_emul/emul_build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
 source ../scripts/async_libsrcs.source
-../scripts/L5P_DRAM_Experiments/setup_server.sh
 sudo rm -rf nginx_gzip_build
 cd nginx-1.23.1
-./configure --prefix=$(pwd)/../nginx_gzip_build  --with-debug --add-module=$(pwd)/../nginx-nmp-overhead-module
+./configure --prefix=$(pwd)/../nginx_gzip_build  --add-module=$(pwd)/../nginx-nmp-overhead-module
 make -j 30
 make -j 30 install 
 cd ../
-cp nginx_default.conf nginx_gzip_build/conf/nginx.conf
+cp ../SmartDIMM_gzip_nginx_conf/nginx_default.conf nginx_gzip_build/conf/nginx.conf
 cp -r localhost.* nginx_gzip_build/conf/
 ps aux | grep nginx | awk '{print $2}' | xargs sudo kill -s 2
 sudo ./nginx_gzip_build/sbin/nginx
-../scripts/L5P_DRAM_Experiments/setup_compression_corpus.sh 16K
+cp UCFile_16K_1.txt nginx_gzip_build/html
--- pqos_test.sh
+++ pqos_test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+export ROOT_DIR=/home/n869p538/wrk_offloadenginesupport/async_nginx_build
+source $ROOT_DIR/scripts/async_libsrcs.source
+
 CORES=( "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" )
-#CORES=( "1" "2"  )
 PIDS=( $(pgrep mcf_r) )
 
 #1 - process to monitor memory bandwidth of
 spec_cpu_mon(){
 	while [ "1" ]; do
 		time=$(printf '%(%H:%M:%S)T')
-		cpu_util=$( top -b -n 2 -d 0.2 -p ${1} | tail -1 | awk '{print $9}' )
-		echo "${time},${cpu_util}" >> mcf_r_${1}.cpu
+		cpu_util=$( top -b -n 2 -d 0.1 -p ${1} 2>/dev/null | tail -1 | awk '{print $6}'  )
+		echo "${time} ${cpu_util}" | grep -v '%CPU' >> mcf_r_${1}.cpu
 		sleep 0.2
 	done
 
@@ -22,10 +24,10 @@ spec_mon(){
 }
 
 sudo pqos -R 
-#sudo pqos -e "llc:1=0x0007;"
+sudo pqos -e "llc:1=0x003F;" #6 ways
 for i in ${CORES[@]};
 do
-	taskset -c ${i} ./cpu_2017/bin/runcpu --dry-run --iterations=1 --copies=1 -o csv 505.mcf_r >/dev/null &
+	taskset -c ${i} ${ROOT_DIR}/cpu_2017/bin/runcpu --iterations=1 --copies=1 -o csv 505.mcf_r >/dev/null &
 done
 
 while [ "${#PIDS[@]}" -lt ${#CORES[@]} ]; do
@@ -35,8 +37,8 @@ done
 echo "got pids: ${PIDS[*]}"
 	
 for p in "${PIDS[@]}"; do
-	#spec_mon $p &
-	#MONS+=( "$!" )
+	spec_mon $p &
+	MONS+=( "$!" )
 	spec_cpu_mon $p &
 	MONS+=( "$!" )
 done
@@ -49,7 +51,7 @@ while [ "${comp}" = 0 ];do
 		echo "checking: $p complete?"
 		kill -0 $p 2>/dev/null
 		comp=$?;
-		sleep 1
+		sleep 0.4
 	done
 done
 
